82a8f572bdc36f73451a307f070fbeae65f83899
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-04 18:20:17 +0100

    retornando API

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,12 +5,6 @@ import { useState, useEffect } from "react";
 // https://api.github.com/orgs/rocketseat/repos
 
 
-const repository = {
-  name: 'unform',
-  description: 'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
-
 
 export function RepositoryList() {
   const [repositories, setRepositories] = useState([]);
@@ -26,10 +20,10 @@ export function RepositoryList() {
     <section className="repository-list">
       <h1>Repository List</h1>
       <ul>
-        <RepositoryItem repository = {repository} />
-        <RepositoryItem repository = {repository} />
-        <RepositoryItem repository = {repository} />
-        <RepositoryItem repository = {repository} />
+        
+        {repositories.map(repository => {
+          return <RepositoryItem  key={repository.name} repository = {repository} />
+        })}
         
         
       </ul>
