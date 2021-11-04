20fb5f0ada584de293c8ab18ae90f062b64639ee
Leandro Camilotti <lecamilotti@gmail.com>
2021-11-04 18:11:06 +0100

    use effect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,4 +1,9 @@
 import { RepositoryItem } from "./RepositoryItem";
+import '../styles/repositories.scss';
+import { useState, useEffect } from "react";
+
+// https://api.github.com/orgs/rocketseat/repos
+
 
 const repository = {
   name: 'unform',
@@ -8,6 +13,14 @@ const repository = {
 
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos')
+    .then(response => response.json())
+    .then(data => setRepositories(data))
+
+  }, []);
 
   return (
     <section className="repository-list">
